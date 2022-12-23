76da2e151a17718540a5f11ae2011d5194a1b6e2
ThuongHoang456189 <ngocanh654189@gmail.com>
2022-12-23 08:58:40 +0700

    #4: Updated Quiz Loading Screen UI with a curve

--- qbox-trial1/screens/quiz-app/QuizLoadingScreen.js
+++ qbox-trial1/screens/quiz-app/QuizLoadingScreen.js
@@ -17,23 +17,78 @@ const QuizLoadingScreen = () => {
         })
     }, []);
 
+    const borderRadius = 150;
+
     return (
         <SafeAreaView style={PlatformSafeView.PlatformSafeArea} className="bg-white">
-            <View className="flex-1 justify-start bg-red-500">
-                <View height={height * 0.5} className="justify-center items-center bg-blue-200">
+            <View className="flex-1 justify-start bg-blue-200">
+                <View
+                    height={height * 0.6}
+                    borderTopRightRadius={120}
+                    borderBottomRightRadius={borderRadius}
+                    className="flex-row items-center justify-center bg-white">
+                    {/*<Animated.View*/}
+                    {/*    height={height}*/}
+                    {/*    borderRadius={80}*/}
+                    {/*    bottom={0.52*height}*/}
+                    {/*    width={0.5*width}*/}
+                    {/*    className="flex-none absolute bg-green-300 p-3">*/}
+
+                    {/*</Animated.View>*/}
                     <View
                         height={verticalScale(250)}
                         width={moderateScale(250)}
-                        className="flex-row bg-white-300 items-center justify-center">
+                        className="flex-row items-center justify-center">
                         <Image source={{
                             uri: "https://links.papareact.com/wru"
-                        }} className="w-full h-full bg-green-400 rounded-full"/>
+                        }} className="w-full h-full"/>
                     </View>
                 </View>
 
-                <Animated.View>
 
-                </Animated.View>
+                {/*<View className="flex-row">*/}
+                {/*    <Animated.View*/}
+                {/*        height={height}*/}
+                {/*        borderRadius={80}*/}
+                {/*        top={0.52*height}*/}
+                {/*        width={0.5*width}*/}
+                {/*        className="flex-none absolute bg-green-300 p-3">*/}
+
+                {/*    </Animated.View>*/}
+                {/*</View>*/}
+
+
+                {/*<View height={height * 0.5} className="justify-center items-center bg-blue-300">*/}
+                {/*    <View*/}
+                {/*        height={verticalScale(250)}*/}
+                {/*        width={moderateScale(250)}*/}
+                {/*        className="flex-row items-center justify-center">*/}
+                {/*        <Image source={{*/}
+                {/*            uri: "https://links.papareact.com/wru"*/}
+                {/*        }} className="w-full h-full"/>*/}
+                {/*    </View>*/}
+                {/*</View>*/}
+
+
+            </View>
+
+            <View
+                height={height * 0.4}
+                borderTopLeftRadius={borderRadius}
+                className="flex-row items-center justify-center bg-blue-200">
+                {/*<Animated.View*/}
+                {/*    height={height}*/}
+                {/*    borderRadius={80}*/}
+                {/*    bottom={0.52*height}*/}
+                {/*    width={0.5*width}*/}
+                {/*    className="flex-none absolute bg-green-300 p-3">*/}
+
+                {/*</Animated.View>*/}
+                <View
+                    height={verticalScale(250)}
+                    width={moderateScale(250)}
+                    className="flex-row items-center justify-center">
+                </View>
             </View>
         </SafeAreaView>
     );
