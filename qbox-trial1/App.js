424a56db31ae12877a2691a037fc8d1246c2dee3
Hiển <hienbui25g@gmail.com>
2023-01-07 18:23:53 +0700

    Add dependency

--- qbox-trial1/App.js
+++ qbox-trial1/App.js
@@ -1,30 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { TailwindProvider } from "tailwindcss-react-native";
-import { NavigationContainer} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import QuizStartingScreen from "./screens/quiz-app/QuizStartingScreen";
-import QuizLoadingScreen from "./screens/quiz-app/QuizLoadingScreen";
-import QuizScreen from "./screens/quiz-app/QuizScreen";
-import SoundScreen from "./screens/quiz-app/SoundScreen";
-import VolcanoScreen from "./screens/volcano-vocab/VolcanoScreen";
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { TailwindProvider } from 'tailwindcss-react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen'
+import QuizStartingScreen from './screens/quiz-app/QuizStartingScreen'
+import QuizLoadingScreen from './screens/quiz-app/QuizLoadingScreen'
+import QuizScreen from './screens/quiz-app/QuizScreen'
+import SoundScreen from './screens/quiz-app/SoundScreen'
+import VolcanoScreen from './screens/volcano-vocab/VolcanoScreen'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-      <NavigationContainer>
-          <TailwindProvider>
-              <Stack.Navigator>
-                  <Stack.Screen name="Home" component={HomeScreen}/>
-                  <Stack.Screen name="QuizStartingScreen" component={QuizStartingScreen}/>
-                  <Stack.Screen name="QuizLoadingScreen" component={QuizLoadingScreen}/>
-                  <Stack.Screen name="QuizScreen" component={QuizScreen}/>
-                  <Stack.Screen name="SoundScreen" component={SoundScreen}/>
-                  <Stack.Screen name="VolcanoScreen" component={VolcanoScreen}/>
-              </Stack.Navigator>
-          </TailwindProvider>
-      </NavigationContainer>
-  );
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="QuizStartingScreen"
+            component={QuizStartingScreen}
+          />
+          <Stack.Screen
+            name="QuizLoadingScreen"
+            component={QuizLoadingScreen}
+          />
+          <Stack.Screen name="QuizScreen" component={QuizScreen} />
+          <Stack.Screen name="SoundScreen" component={SoundScreen} />
+          <Stack.Screen name="VolcanoScreen" component={VolcanoScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+  )
 }
--- qbox-trial1/screens/volcano-vocab/VolcanoScreen.js
+++ qbox-trial1/screens/volcano-vocab/VolcanoScreen.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import {Text, View} from "react-native";
-import AnimatedLottieView from "lottie-react-native";
+import React, { useRef, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Canvas, useRender, useFrame } from '@react-three/fiber'
 
-const VolcanoScreen = () => {
-    const animation = React.useRef(null);
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
 
-    React.useEffect(() => {
-        animation.current.play(0, 200);
-    }, [])
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
 
-    return (
-        <View>
-            <AnimatedLottieView
-                ref={animation}
-                style={{width: 100, height: 100, marginLeft: 20}}
-                source={require('qbox-trial1/assets/lottie/temp/coins.json')}
-                autoPlay={false}
-                loop={false}
-            />
-        </View>
-    );
-};
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
 
-export default VolcanoScreen;
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
+    >
+      <boxGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? 'hotpink' : 'orange'}
+      />
+    </mesh>
+  )
+}
+
+export default function VolcanoScreen() {
+  return (
+    <View style={styles.container}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+})
